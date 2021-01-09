18362bfdd8fd2cee69d5116638c330987c72e1bb
shijie666 <chudonghai@126.com>
2021-01-09 22:01:24 +0800

    spring-boot and thymeleaf bootstrap

---
+++ spring-boot-bootstrap/README.md
@@ -0,0 +1,4 @@
+# spring boot 整合 thymeleaf和bootstrap
+
+参考：
+- [1] [spring.io](https://spring.io/guides)
---
+++ spring-boot-bootstrap/src/main/java/com/demo/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/index")
+    public String index(@RequestParam(name="name", required = false, defaultValue = "zhangsan")
+                                    String name, Model model){
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
